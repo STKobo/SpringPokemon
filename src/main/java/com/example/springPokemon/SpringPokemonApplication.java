9ec86e348640bd151ae500845dbe762b69c0b837
Stephane_tcha <stephanetcha@gmail.com>
2023-02-06 19:59:56 +0100

    first commit

---
+++ src/main/java/com/example/springPokemon/SpringPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springPokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springPokemon/SpringPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springPokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
