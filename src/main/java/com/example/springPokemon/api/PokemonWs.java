0cbe6b223ae09e3bbf6e92bafa4da7a6c02a4fb0
Stephane_tcha <stephanetcha@gmail.com>
2023-02-07 12:21:11 +0100

    Put and Delete

--- src/main/java/com/example/springPokemon/api/PokemonWs.java
+++ src/main/java/com/example/springPokemon/api/PokemonWs.java
@@ -2,8 +2,11 @@ package com.example.springPokemon.api;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,15 @@ public class PokemonWs {
     @PostMapping
     public void createPokemon(@RequestBody Pokemon pokemon){
         return null,
+    }
+
+    @PutMapping("{name}")
+    public void updatePokemon(@PathVariable(name = "name") String nom){
+
+    }
+
+    @DeleteMapping("{name}")
+    public void deletePokemon(@PathVariable(name = "name") String nom){
+
     }
 }
