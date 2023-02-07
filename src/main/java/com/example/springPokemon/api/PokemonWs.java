2df8f66091ae625cdcd6a524a4c30c2910e08f31
Stephane_tcha <stephanetcha@gmail.com>
2023-02-07 12:16:27 +0100

    Post

--- src/main/java/com/example/springPokemon/api/PokemonWs.java
+++ src/main/java/com/example/springPokemon/api/PokemonWs.java
@@ -3,6 +3,8 @@ package com.example.springPokemon.api;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,8 @@ public class PokemonWs {
         return null;
     }
 
-    
+    @PostMapping
+    public void createPokemon(@RequestBody Pokemon pokemon){
+        return null,
+    }
 }
